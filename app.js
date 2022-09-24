8564fda2466b1b216da99ca22c653530892409ff
ManideepKadambari-Dev <manideep.kadambari.official@gmail.com>
2022-09-25 01:00:44 +0530

    Commit for surya

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-console.log("Mandy");
-console.log("Third change");
-=======
-console.log("Mandy");
->>>>>>> parent of 94afef4 (dev branch)
-=======
-console.log("Mandy");
->>>>>>> parent of 94afef4 (dev branch)
+console.log("Hello ra surya");
